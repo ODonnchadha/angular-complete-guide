7c14721c1dfce46d8605e10942626f57fbf904ef
ODonnchadha <danieldonahue1231@hotmail.com>
2020-03-21 09:40:28 -0500

    Addition of pseudo navigation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-complete-guide';
+  title = 'angular-complete-guide::TDS Polymer';
+  
+  currentSelector = 'recipes';
+  onNavigate(selector : string) {
+    this.currentSelector = selector;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+    @Output() selectedNavigation : EventEmitter<string> = new EventEmitter<string>();
 
+    onNavigationSelection(selector: string) {
+        this.selectedNavigation.emit(selector);
+    }
 }
