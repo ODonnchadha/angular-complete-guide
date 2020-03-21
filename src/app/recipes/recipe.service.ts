07a7d5a759422bc03af250b3f57ff1148a26d8a5
ODonnchadha <danieldonahue1231@hotmail.com>
2020-03-21 15:28:47 -0500

    Adding ingredients array to the recipe model.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
@@ -6,11 +7,15 @@ export class RecipeService {
         new Recipe(
           'Protein', 
           'Decent tasting stuff. Highly underrated.', 
-          'https://www.stickpng.com/assets/images/5a01c454cad612fd1571181b.png'
+          'https://www.stickpng.com/assets/images/5a01c454cad612fd1571181b.png', [
+            new Ingredient('Guinness', 100),
+            new Ingredient('Jameson', 12),           
+          ]
         ), new Recipe(
           'Fatty Acids', 
           'Great tasting. Wonderous. And probably quite amazing.', 
-          'https://image.shutterstock.com/z/stock-vector-fa-letters-four-colors-in-abstract-background-logo-design-identity-in-circle-alphabet-letter-417449098.jpg'
+          'https://image.shutterstock.com/z/stock-vector-fa-letters-four-colors-in-abstract-background-logo-design-identity-in-circle-alphabet-letter-417449098.jpg',[
+          ]
         )
       ];
 
